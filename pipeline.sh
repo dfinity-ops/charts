2402c17d3741cc9682f065ee637d22c1624124a5
Evans Tucker <gmail@evanstucker.com>
2025-06-29 10:01:37 -0700

    ...

--- pipeline.sh
+++ pipeline.sh
@@ -9,22 +9,22 @@ arg="${1:-}"
 
 echo "$helm_password" | helm registry login -u "$helm_username" --password-stdin "$helm_host"
 
-#helm lint dfinity-ops/*
-helm lint dfinity-ops/app
+#helm lint devopscoop/*
+helm lint devopscoop/app
 
-#for chart in dfinity-ops/*; do
-for chart in dfinity-ops/app; do
+#for chart in devopscoop/*; do
+for chart in devopscoop/app; do
   chart_name=$(echo "$chart" | cut -d/ -f2)
   chart_version=$(grep '^version: ' "${chart}/Chart.yaml" | cut -d' ' -f 2)
 
   helm package "${chart}"
 
   # If chart already exists in the chart repository, don't push.
-  if helm pull "oci://ghcr.io/dfinity-ops/charts/${chart_name}" --version "${chart_version}" &> /dev/null; then
+  if helm pull "oci://ghcr.io/devopscoop/charts/${chart_name}" --version "${chart_version}" &> /dev/null; then
     echo -e "\e[31mWARNING: Chart ${chart_name} version ${chart_version} already exists in the repository.\nThis means that the chart's code has not changed, or you forgot to update the version in Chart.yaml.\e[0m"
   else
     if [[ $arg == 'push' ]]; then
-      helm push "${chart_name}-${chart_version}.tgz" "oci://ghcr.io/dfinity-ops/charts"
+      helm push "${chart_name}-${chart_version}.tgz" "oci://ghcr.io/devopscoop/charts"
     fi
   fi
 done
